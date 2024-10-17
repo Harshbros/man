0d3e5eb7e52bad42bb5e3fdb301e4c64cce1355c
Harsh <harshitsinghkohli@gmail.com>
2024-10-17 19:35:35 -0400

    Initial Commit

---
+++ man/manApp.swift
@@ -0,0 +1,17 @@
+//
+//  manApp.swift
+//  man
+//
+//  Created by Harshit Singh Kohli on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct manApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ manTests/manTests.swift
@@ -0,0 +1,17 @@
+//
+//  manTests.swift
+//  manTests
+//
+//  Created by Harshit Singh Kohli on 2024-10-17.
+//
+
+import Testing
+@testable import man
+
+struct manTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
